bf173fef565e3a1a556f0f891e14a9d5863d86b2
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-06-10 20:33:48 +0530

    Create Profile window interface

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -13,7 +13,7 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("view/Order.fxml")));
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("view/Profile.fxml")));
         scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         //stage.initStyle(StageStyle.TRANSPARENT);
